1cdaf827f367b34e9a0efd0448973367fd5c39b5
Frank67Wu <f67wu@uwaterloo.ca>
2022-05-08 15:49:57 -0400

    Added board display

---
+++ chessBoard.js
@@ -0,0 +1,35 @@
+function addSquares() {
+    for (let i = 0; i < 8; i++) {
+        for (let j = 0; j < 8; j++) {
+            if ((j + i) % 2 == 0) {
+                let whiteSquare = document.createElement('div');
+                whiteSquare.classList.add('whiteSquare');
+                document.getElementById('board').children.item(i).appendChild(whiteSquare);
+            }
+
+            else {
+                let blackSquare = document.createElement('div');
+                blackSquare.classList.add('blackSquare');
+                document.getElementById('board').children.item(i).appendChild(blackSquare);
+            }
+        }
+    }
+}
+
+
+function displayBoard() {
+    let chessBoard= document.createElement('div');
+    chessBoard.setAttribute('id', 'board');
+    document.body.appendChild(chessBoard);
+
+    for (let i = 0; i < 8; i++) {
+        let boardRow = document.createElement('div');
+        boardRow.classList.add('row');
+        chessBoard.appendChild(boardRow);
+    }
+
+    addSquares();
+
+}
+
+displayBoard();
